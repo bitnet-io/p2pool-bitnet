52f499d8b032860ee268a90270b9a27f0c91ec22
Your Name <you@example.com>
2023-12-26 23:36:11 +0000

    attempting to fix SUBSIDY for 10,000 BIT

--- p2pool/bitcoin/networks/bitnet_testnet.py
+++ p2pool/bitcoin/networks/bitnet_testnet.py
@@ -1,10 +1,12 @@
+
 import os
 import platform
 
 from twisted.internet import defer
 
 from .. import data, helper
-from p2pool.util import pack
+from p2pool.util import math, pack, jsonrpc
+
 
 
 P2P_PREFIX = '0b110907'.decode('hex')
@@ -17,25 +19,28 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
             'getreceivedbyaddress' in (yield bitcoind.rpc_help()) and
             (yield bitcoind.rpc_getblockchaininfo())['chain'] == 'test'
         ))
+#10,000 BIT subsidy
 SUBSIDY_FUNC = lambda height: 10000*100000000 
+
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('aurum_hash').getPoWHash(data))
-BLOCK_PERIOD = 600 # s
+BLOCK_PERIOD = 210 # s
 SYMBOL = 'TB'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'bitnet') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/bitnet/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bitnet'), 'bitnet.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://blockexplorer.com/testnet/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'http://blockexplorer.com/testnet/address/'
 TX_EXPLORER_URL_PREFIX = 'http://blockexplorer.com/testnet/tx/'
 
-#SANE_TARGET_RANGE = (2**256//1000000000000000 - 1, 2**256//1000 - 1)
-#DUMB_SCRYPT_DIFF = 2**16
-#DUST_THRESHOLD = 0.03e8
 
 #SANE_TARGET_RANGE =  (2**256//2**32//1000000000 - 1, 2**256//2**32 - 1)  # TINY SHARES HERE
 #SANE_TARGET_RANGE =  (2**256//2**4//1000000000 - 1, 2**256//1000 - 1)  # TINY SHARES HERE
 
-#SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**4 - 1)
+#SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
+#DUMB_SCRYPT_DIFF = 2**3
+#DUST_THRESHOLD = 0.001e8
+
 
 
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
-DUMB_SCRYPT_DIFF = 1
+#DUMB_SCRYPT_DIFF = 2**16
+DUMB_SCRYPT_DIFF = 2**2
 DUST_THRESHOLD = 0.001e8
--- run-testnet.sh
+++ run-testnet.sh
@@ -1,2 +1,2 @@
-python2 run_p2pool.py  --allow-obsolete-bitcoind --testnet --net bitnet -a mffRRtFXoH5bGvpGFX6DJLZBpmAwemmyJ1 --give-author 0 --worker-port 80 --bitcoind-p2p-port 9999 --bitcoind-rpc-port 18332 testuser testpass
+python2 run_p2pool.py  --allow-obsolete-bitcoind --testnet --net bitnet -a tb1qh9uj6sx6806ytgtr9jkl5yaq392t4k4tgs2zdn --give-author 0 --worker-port 80 --bitcoind-p2p-port 9999 --bitcoind-rpc-port 18332 testuser testpass
  
